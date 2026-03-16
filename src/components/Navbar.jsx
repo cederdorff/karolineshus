48d15d2a790445bdccf6138a4118a0269a3725af
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2026-03-17 00:05:21 +0100

    Implement mobile navigation toggle and update styles for better responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,80 @@
-import { Link, NavLink } from "react-router";
+import { useEffect, useState } from "react";
+import { Link, NavLink, useLocation } from "react-router";
 import { contactInfo, logoUrl } from "../siteContent";
 
 export default function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setIsMobileMenuOpen(false);
+  }, [location.pathname]);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen((current) => !current);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
     <header id="masthead" className="site-header" role="banner">
       <div className="site-topbar">
         <div className="container">
           <nav className="main-navigation" aria-label="Hovednavigation">
-            <div className="menu-primary-container">
+            <button
+              className="menu-toggle"
+              type="button"
+              aria-expanded={isMobileMenuOpen}
+              aria-controls="primary-menu"
+              aria-label={isMobileMenuOpen ? "Luk menu" : "Åbn menu"}
+              onClick={toggleMobileMenu}
+            >
+              <span className="menu-toggle__icon" aria-hidden="true">
+                <span />
+                <span />
+                <span />
+              </span>
+              <span className="menu-toggle__label">Menu</span>
+            </button>
+
+            <div
+              className={`menu-primary-container${isMobileMenuOpen ? " is-open" : ""}`}
+            >
               <ul id="primary-menu" className="menu">
                 <li>
-                  <NavLink to="/" end>
+                  <NavLink to="/" end onClick={closeMobileMenu}>
                     Hjem
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/udstillinger">Udstillinger</NavLink>
+                  <NavLink to="/udstillinger" onClick={closeMobileMenu}>
+                    Udstillinger
+                  </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/kunstnere">Kunstnere</NavLink>
+                  <NavLink to="/kunstnere" onClick={closeMobileMenu}>
+                    Kunstnere
+                  </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/om">Om os</NavLink>
+                  <NavLink to="/om" onClick={closeMobileMenu}>
+                    Om os
+                  </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/kunstnere/ellen-bye-jensen">
+                  <NavLink
+                    to="/kunstnere/ellen-bye-jensen"
+                    onClick={closeMobileMenu}
+                  >
                     Ellen Bye Jensen
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/kontakt">Kontakt</NavLink>
+                  <NavLink to="/kontakt" onClick={closeMobileMenu}>
+                    Kontakt
+                  </NavLink>
                 </li>
               </ul>
             </div>
